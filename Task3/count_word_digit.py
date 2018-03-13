b208dc8907476ef54e08490af159befca08c34ab
vipinpaul <vipin.paul@bridgeconn.com>
2018-03-13 13:20:29 +0530

    Task set 3 completed

---
+++ Task3/count_word_digit.py
@@ -0,0 +1,17 @@
+#Accepts a sentence which will have word and digit 
+#and calculate the count of letters and digits.
+
+
+string = input("Enter a combination of word and digit=")
+print(string)
+alphabet = 0
+digit = 0
+
+for i in string:
+	if(i.isalpha()):
+		alphabet += 1
+	elif(i.isdigit()):
+		digit += 1
+
+print("Count of letters=", alphabet)
+print("Count of digits=", digit)
