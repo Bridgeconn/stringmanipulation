c44fbc0e15c7fa9c625bc9c0e11d8c9f7cedbb81
vipinpaul <vipin.paul@bridgeconn.com>
2018-04-05 17:31:42 +0530

    Comparison of Old and New translation, adding a column to show the difference

--- comparison of old and new translation/old_new_comp.py
+++ comparison of old and new translation/old_new_comp.py
@@ -1,3 +1,5 @@
+# To compare the old and new translations in a same file and 
+# write the difference into next column 
 
 
 import openpyxl
@@ -8,7 +10,7 @@ import re
 files = sorted(glob.glob("*.xlsx"))
 
 for file in files:
-	print(file)
+	# Loading the excel file
 	load_file = openpyxl.load_workbook(file)
 	file_sheets = load_file.get_sheet_names()
 	file_sheet = load_file.get_sheet_by_name(file_sheets[0])
@@ -16,49 +18,47 @@ for file in files:
 
 	cwd = os.getcwd()
 	file_path = cwd + "/" + file
+	# Giving the column heading manually
 	file_sheet['F' + str(1)] = "Difference"
 
 	for rows in range(2, file_sheet.max_row + 1):
-		
+		# Reading the verses row by row
 		new_verse = file_sheet['D' + str(rows)].value
-		#print(new_verse)
 		old_verse = file_sheet['E' + str(rows)].value
-		#print(old_verse)
+		
 		dif_ver = []
 		differences = ""
 		if new_verse:
 			new_versus = new_verse.split("\n•")
-			#print(new_versus)
+		
 			if old_verse:
 				old_versus = old_verse.split("\n•")
 
 				for new in new_versus:
-					#print(new)
+					# Fetching the verse line by line
 					if new:
 						for old in old_versus:
 							if old:
 								if new == old:
 									print("")
-									#print("New-------",new)
-									#print("Old-------",old)
+			
 								else:
 									if "•"+ new != "\n":
 										if new not in dif_ver:
+											# Adding the content into list
 											dif_ver = "•"+ new
 							else:
-								#if "•"+ new != "\n":
 								if new not in dif_ver:
 									dif_ver = "•"+ new
 						differences += dif_ver
-				
+				# Aligning the content
 				diff = re.sub(r'•','\n•',differences)
-				#print(diff,rows)
+				# Writing into excel
 				file_sheet['F' + str(rows)] = diff
 				load_file.save(file_path)
 
 			else:
 				new = file_sheet['D' + str(rows)].value
-				#print(new,rows)
-				#new = new_versus
+				
 				file_sheet['F' + str(rows)] = new
 				load_file.save(file_path)
