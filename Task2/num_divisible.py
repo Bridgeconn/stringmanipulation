3069c14997c73eec0294337efcf40292ada05e4f
vipinpaul <vipin.paul@bridgeconn.com>
2018-06-20 16:26:12 +0530

    Old

--- Task2/num_divisible.py
+++
@@ -1,9 +0,0 @@
-# Find all numbers which should divisible by 7 but are not a multiple of 5, 
-# between 900 and 2000.
-
-
-x=900
-while x <= 2000:
-	if( x%7 == 0 and x%5 != 0):
-		print(x)
-	x += 1 
